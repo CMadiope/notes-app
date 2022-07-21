4e823dc4abcaac380948e404b039bcc63bb2d85a
Confidence Madiope <confidencem97@gmail.com>
2022-07-21 13:08:29 +0200

    ADD: get text and save text functions

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './Components/css/App.css'
 import Header from './Components/NoteComponents/Header';
 import Notes from './Components/NoteComponents/Notes';
 
+
 function App() {
   return (
     <div className="main">
--- src/Components/NoteComponents/Notes.js
+++ src/Components/NoteComponents/Notes.js
@@ -1,7 +1,8 @@
 import React,{useState} from 'react'
 import Note from './Note'
 import '../css/Note.css'
-
+import CreateNote from './CreateNote'
+import {v4 as uuid} from 'uuid'
 
 
 const Notes = () => {
@@ -9,11 +10,29 @@ const Notes = () => {
   const [notes, setNotes] = useState([]);
   const [inputText, setInputText] = useState("");
 
+  // get text and store in state
+  const textHandler = e => {
+    setInputText(e.target.value)
+  }
+
+  // add new note to the state array
+  const saveHandler = () => {
+    setNotes((prevState) => [
+      ...prevState,
+      {
+        id: uuid(),
+        text: inputText,
+      },
+    ])
+    //clear textarea
+    setInputText('')
+  }
+
   return (
     <div className='notes'>
       <Note/>
       <Note/>
-      <Note/>
+      <CreateNote/>
     </div>
   )
 }
