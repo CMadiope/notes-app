28a315267623ea2feb95626e3b8f59b9b7413ffd
Confidence Madiope <confidencem97@gmail.com>
2022-07-21 13:17:22 +0200

    call the props in CreateNote component

--- src/Components/NoteComponents/CreateNote.js
+++ src/Components/NoteComponents/CreateNote.js
@@ -1,17 +1,19 @@
 import React from "react";
 
-const CreateNote = () => {
+const CreateNote = ({textHandler, saveHandler, inputText}) => {
   return (
     <div className='note' style={{ background: "rgba(255, 255, 255, 0)" }}>
       <textarea
         cols='10'
         rows='5'
+        value={inputText}
         placeholder='Type...'
+        onChange={textHandler}
         maxLength='100'
       ></textarea>
       <div className="note__footer">
         <span className="label">left</span>
-        <button className="none__save">Save</button>
+        <button className="none__save" onClick={saveHandler}>Save</button>
       </div>
     </div>
   );
