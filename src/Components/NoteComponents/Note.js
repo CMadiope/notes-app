03a74f185b2a00c8172e743813da44b3617d59d3
Confidence Madiope <confidencem97@gmail.com>
2022-07-21 13:29:58 +0200

    ADD: text, delete funtion to Note body

--- src/Components/NoteComponents/Note.js
+++ src/Components/NoteComponents/Note.js
@@ -2,12 +2,14 @@ import React from 'react'
 import DeleteForeverOutlinedIcon from "@mui/icons-material/DeleteForeverOutlined";
 // import '../css/Note.css'
 
-const Note = () => {
+const Note = ({id, text, deleteNote}) => {
   return (
     <div className='note'>
-    <div className="note__body"></div>
+    <div className="note__body">{text}</div>
     <div className="note__footer" style={{justifyContent: 'flex-end'}}>
-      <DeleteForeverOutlinedIcon className='note__delete' aria-hidden='true'></DeleteForeverOutlinedIcon>
+      <DeleteForeverOutlinedIcon className='note__delete' 
+      onClick={() => deleteNote(id)}
+      aria-hidden='true'></DeleteForeverOutlinedIcon>
     </div>
     </div>
   )
